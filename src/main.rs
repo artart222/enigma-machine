478618ff32264319acf7692a3b68312287bc6261
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2024-12-29 00:16:59 +0330

    Reflector has been added

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@
 // ==> plugboard ==> lamp
 
 // Alphabet    ABCDEFGHIJKLMNOPQRSTUVWXYZ
+//
+// UKW-A       EJMZALYXVBWFCRQUONTSPIKHGD
+// UKW-B       YRUHQSLDPXNGOKMIEBFZCWVJAT
+// UKW-C       FVPJIAOYEDRZXWGCTKUQSBNMHL
+//
 // Rotor1      EKMFLGDQVZNTOWYHXUSPAIBRCJ   Q ==> R
 // Rotor2      AJDKSIRUXBLHWTMCQGZNPYFVOE   E ==> F
 // Rotor3      BDFHJLCPRTXVZNYEIWGAKMUSQO   V ==> W
+//
 // Hello ==> H ==> D ==> K ==> X
 //           E ==> M ==> W ==> U
 //           L ==> V ==> Y ==> Q
@@ -49,6 +55,51 @@ fn main() {
         ('j', 'i'),
     ]);
 
+    // TODO: Check if is better way to do this.
+    let reflector: HashMap<char, char> = HashMap::from([
+        //             A <==> E
+        //             B <==> J
+        //             C <==> M
+        //             D <==> Z
+        //             F <==> L
+        //             G <==> Y
+        //             H <==> X
+        //             I <==> V
+        //             K <==> W
+        //             N <==> R
+        //             Q <==> O
+        //             P <==> U
+        //             S <==> T
+        //
+        ('a', 'e'),
+        ('b', 'j'),
+        ('c', 'm'),
+        ('d', 'z'),
+        ('f', 'l'),
+        ('g', 'y'),
+        ('h', 'x'),
+        ('i', 'v'),
+        ('k', 'w'),
+        ('n', 'r'),
+        ('q', 'o'),
+        ('p', 'u'),
+        ('s', 't'),
+        //
+        ('e', 'a'),
+        ('j', 'b'),
+        ('m', 'c'),
+        ('z', 'd'),
+        ('l', 'f'),
+        ('y', 'g'),
+        ('x', 'h'),
+        ('v', 'i'),
+        ('w', 'k'),
+        ('r', 'n'),
+        ('o', 'q'),
+        ('u', 'p'),
+        ('t', 's'),
+    ]);
+
     let mut rotor1 = Rotor {
         letters_list: [
             'e', 'k', 'm', 'f', 'l', 'g', 'd', 'q', 'v', 'z', 'n', 't', 'o', 'w', 'y', 'h', 'x',
@@ -80,31 +131,30 @@ fn main() {
     for char in input.chars() {
         let mut new_char = char;
 
-        // let mut new_char: char;
-        // match plugboard.get(&char) {
+        // match plugboard.get(&new_char) {
         //     Some(charachter) => new_char = *charachter,
         //     _ => new_char = char,
         // }
         // println!("{}", new_char);
         // First plugboard.
 
-        let mut rotor1_index = alphabet.iter().position(|&r| r == new_char).unwrap() as usize;
+        let rotor1_index = alphabet.iter().position(|&r| r == new_char).unwrap() as usize;
 
         rotor1.current_number += 1;
-        if (rotor1.current_number == 26) {
+        if rotor1.current_number == 26 {
             rotor1.current_number = 0;
         }
 
-        if (rotor1.notch == rotor1.current_number) {
+        if rotor1.notch == rotor1.current_number {
             rotor2.current_number += 1;
-            if (rotor2.current_number == 26) {
+            if rotor2.current_number == 26 {
                 rotor2.current_number = 0;
             }
         }
 
-        if (rotor2.notch == rotor2.current_number) {
+        if rotor2.notch == rotor2.current_number {
             rotor3.current_number += 1;
-            if (rotor3.current_number == 26) {
+            if rotor3.current_number == 26 {
                 rotor3.current_number = 0;
             }
         }
@@ -118,8 +168,13 @@ fn main() {
         new_char = rotor2.letters_list[rotor2_index - rotor2.current_number];
         let rotor3_index = alphabet.iter().position(|&r| r == new_char).unwrap() as usize;
         new_char = rotor3.letters_list[rotor3_index - rotor3.current_number];
+        // End of first phase of reflector.
 
-        // TODO: Here should be reflector code.
+        match reflector.get(&new_char) {
+            Some(charachter) => new_char = *charachter,
+            _ => new_char = char,
+        }
+        // End of reflector code.
 
         // TODO: Here should be second phase of reflector.
 
